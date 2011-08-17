08ed31518f04a622d1582df0714e0bd9ce490122
Scott Kruger <scott@chojin.org>
2011-08-17 08:40:23 -0500

    added tests for query and yql-query
    Added some tests for query and yql-query to yahoo/query-test.

--- test/yahoo/query_test.clj
+++ test/yahoo/query_test.clj
@@ -1,13 +1,15 @@
 (ns yahoo.query-test
   (:use clojure.test
         [yahoo.query :only [query yql-query]]
+        [yahoo.yql   :only [select table where]]
         :reload-all))
 
 (deftest ^{:doc "Query tests"}
   query-test
   (let [goog  "http://www.google.com"
         fs    "http://fantasysports.yahooapis.com/fantasy/v2"
-        pms   {:use_login 1}]
+        pms   {:use_login 1}
+        more  (merge pms {:a 2 :b "three"})]
     
     (is (= [(str goog "/test") nil]
            (query goog :test)))
@@ -27,6 +29,27 @@
     (is (= [(str goog "/search") {:q "test"}]
            (query goog [:search* {:q "test"}])))
     
-    (is (= [(str goog "/nonsense;a=1/foo") {:q "test"}]
+    (is (= [(str goog "/nonsense;a=1/foo") {:use_login 1}]
            (query goog [:nonsense {:a 1}] [:foo* pms])))
+
+    (is (= [(str goog "/nonsense;a=1/foo") {:use_login 1}]
+           (query goog [:nonsense {:a 1}] [:foo* pms] :extraneous :keywords)))
+    
+    (is (= [(str goog "/one/nonsense;a=1/two/foo") {:use_login 1}]
+           (query goog :one [:nonsense {:a 1}] :two [:foo* pms])))
+    
+    (is (= [(str goog "/test;b=three;a=2;use_login=1/foo") more]
+           (query goog [:test more] [:foo* more])))
+    ))
+
+(deftest ^{:doc "YQL query tests"}
+  yql-query-test
+  (let [test-q  (select (table :test))
+        yql-url "http://query.yahooapis.com/v1"]
+
+    (is (= [(str yql-url "/yql") {:q test-q}]
+           (yql-query test-q)))
+
+    (is (= [(str yql-url "/yql") {:q "SELECT cols FROM table WHERE id = '4'"}]
+           (yql-query (select (table :table [:cols]) (where (= :id 4))))))
     ))
